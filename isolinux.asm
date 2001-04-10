ec29325fac1d112b3819163f5fb9c39fa05dcad1
hpa <hpa>
2001-04-10 00:01:02 +0000

    Fix for register corruption while displaying F-key help files.

--- isolinux.asm
+++ isolinux.asm
@@ -1453,7 +1453,9 @@ show_help:	; AX = func key # (0 = F1, 9 = F10)
 		add di,FKeyName
 		call searchdir
 		jz fk_nofile
+		push si
 		call crlf
+		pop si
 		call get_msg_file
 		jmp short fk_wrcmd
 fk_nofile:
