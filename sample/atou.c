36166b159309632a2f5485f5312952e2c97bcc63
hpa <hpa>
2003-12-10 20:44:57 +0000

    Split atou() and skip_atou() into separate modules

--- sample/atou.c
+++ sample/atou.c
@@ -4,15 +4,6 @@ isdigit(int ch)
   return (ch >= '0') && (ch <= '9');
 }
 
-unsigned int skip_atou(const char **s)
-{
-  int i=0;
-  
-  while (isdigit(**s))
-    i = i*10 + *((*s)++) - '0';
-  return i;
-}
-
 unsigned int atou(const char *s)
 {
   unsigned int i = 0;
