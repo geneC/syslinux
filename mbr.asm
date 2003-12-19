dae725c9a5e881718f87d11d76e046615ce60c92
hpa <hpa>
2003-12-19 19:31:52 +0000

    Change to MIT license; document offset 440 issue.

--- mbr.asm
+++ mbr.asm
@@ -1,12 +1,27 @@
-; -*- fundamental -*- (asm-mode sucks)
-; $Id$
 ; -----------------------------------------------------------------------
 ;   
-;   NOT Copyright 2000 H. Peter Anvin
+;   Copyright 2003 H. Peter Anvin - All Rights Reserved
 ;
-;   This file is in the public domain.  Enjoy.  However, I would
-;   appreciate it if modified versions were marked as such, and
-;   please don't bug me about any version not released by me.
+;   Permission is hereby granted, free of charge, to any person
+;   obtaining a copy of this software and associated documentation
+;   files (the "Software"), to deal in the Software without
+;   restriction, including without limitation the rights to use,
+;   copy, modify, merge, publish, distribute, sublicense, and/or
+;   sell copies of the Software, and to permit persons to whom
+;   the Software is furnished to do so, subject to the following
+;   conditions:
+;   
+;   The above copyright notice and this permission notice shall
+;   be included in all copies or substantial portions of the Software.
+;   
+;   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+;   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+;   OTHER DEALINGS IN THE SOFTWARE.
 ;
 ; -----------------------------------------------------------------------
 
@@ -198,7 +213,8 @@ bad_disk_msg	db 'Operating system loading error', 13, 10, 0
 ;
 ; Maximum MBR size: 446 bytes; end-of-boot-sector signature also needed.
 ; Note that some operating systems (NT, DR-DOS) put additional stuff at
-; the end of the MBR, so shorter is better.
+; the end of the MBR, so shorter is better.  Location 440 is known to
+; have a 4-byte attempt-at-unique-ID for some OSes.
 ;
 
 PartitionTable	equ $$+446			; Start of partition table
