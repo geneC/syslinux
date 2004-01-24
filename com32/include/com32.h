834e9d5623967dfa7c04b573a72901e24e484ffd
hpa <hpa>
2004-01-24 21:11:47 +0000

    Add explicit defines for the various flags

--- com32/include/com32.h
+++ com32/include/com32.h
@@ -46,6 +46,25 @@ typedef struct {
   reg32_t eflags;		/* Offset 40 */
 } com32sys_t;
 
+/* EFLAGS definitions */
+#define EFLAGS_CF		0x00000001
+#define EFLAGS_PF		0x00000004
+#define EFLAGS_AF		0x00000010
+#define EFLAGS_ZF		0x00000040
+#define EFLAGS_SF		0x00000080
+#define EFLAGS_TF		0x00000100
+#define EFLAGS_IF		0x00000200
+#define EFLAGS_DF		0x00000400
+#define EFLAGS_OF		0x00000800
+#define EFLAGS_IOPL		0x00003000
+#define EFLAGS_NT		0x00004000
+#define EFLAGS_RF		0x00010000
+#define EFLAGS_VM		0x00020000
+#define EFLAGS_AC		0x00040000
+#define EFLAGS_VIF		0x00080000
+#define EFLAGS_VIP		0x00100000
+#define EFLAGS_ID		0x00200000
+
 extern struct com32_sys_args {
   uint32_t cs_sysargs;
   char *cs_cmdline;
