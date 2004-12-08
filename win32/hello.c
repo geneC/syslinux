06907c4ce4e2a695a6607c2f8940e5723b2db57d
hpa <hpa>
2004-12-08 20:22:17 +0000

    C compiler test file

---
+++ win32/hello.c
@@ -0,0 +1,13 @@
+/*
+ * Test program for C compiler; if this doesn't compile, the
+ * C compiler is seriously broken.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hello, World!\n");
+  return 0;
+}
