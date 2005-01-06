3113533310816ae83447ac3b136ac514fad89cc1
hpa <hpa>
2005-01-06 22:44:57 +0000

    Add missing header file

---
+++ com32/lib/pci/pci.h
@@ -0,0 +1,14 @@
+/*
+ * pci/pci.h
+ *
+ * Common internal header file
+ */
+
+#ifndef PCI_PCI_H
+
+#include <sys/pci.h>
+#include <sys/cpu.h>
+
+extern enum pci_config_type __pci_cfg_type;
+
+#endif /* PCI_PCI_H */
