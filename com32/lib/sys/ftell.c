f8edb0e82019cd36dc20abafda23ca523ccc0c13
hpa <hpa>
2005-01-10 02:44:17 +0000

    ftell() operation

---
+++ com32/lib/sys/ftell.c
@@ -0,0 +1,18 @@
+/*
+ * sys/ftell.c
+ *
+ * We can't seek, but we can at least tell...
+ */
+
+#include <stdio.h>
+#include "sys/file.h"
+
+long ftell(FILE *stream)
+{
+  int fd = fileno(stream);
+  struct file_info *fp = &__file_info[fd];
+  
+  return fp->i.offset;
+}
+
+  
