18a2d5163b981425521fa73e9182702aa0e93294
hpa <hpa>
2005-05-19 21:30:50 +0000

    gcc4 compilation fix

---
+++ com32/lib/putchar.c
@@ -0,0 +1,16 @@
+/*
+ * putchar.c
+ *
+ * gcc "printf decompilation" expects this to exist...
+ */
+
+#include <stdio.h>
+
+#undef putchar
+
+int putchar(int c)
+{
+  unsigned char ch = c;
+
+  return _fwrite(&ch, 1, stdout) == 1 ? ch : EOF;
+}
