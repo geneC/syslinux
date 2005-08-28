e0c7f8a022138a4d52a2ae64c5e990a2f4f436d5
hpa <hpa>
2005-08-28 06:00:54 +0000

    Revert USE_PXE_PROVIDED_STACK change.

--- pxelinux.asm
+++ pxelinux.asm
@@ -52,7 +52,7 @@ TFTP_LARGEBLK	equ (TFTP_MTU-20-8-4)	; MTU - IP hdr - UDP hdr - TFTP hdr
 ; Standard TFTP block size
 TFTP_BLOCKSIZE_LG2 equ 9		; log2(bytes/block)
 TFTP_BLOCKSIZE	equ (1 << TFTP_BLOCKSIZE_LG2)
-%assign USE_PXE_PROVIDED_STACK 0	; Use stack provided by PXE?
+%assign USE_PXE_PROVIDED_STACK 1	; Use stack provided by PXE?
 
 SECTOR_SHIFT	equ TFTP_BLOCKSIZE_LG2
 SECTOR_SIZE	equ TFTP_BLOCKSIZE
@@ -357,7 +357,14 @@ _start1:
 
 		; Uh-oh, not there... try plan B
 		mov ax, 5650h
+%if USE_PXE_PROVIDED_STACK == 0
+		lss sp,[InitStack]
+%endif
 		int 1Ah					; May trash regs
+%if USE_PXE_PROVIDED_STACK == 0
+		lss esp,[BaseStack]
+%endif
+
 		jc no_pxe
 		cmp ax,564Eh
 		jne no_pxe
