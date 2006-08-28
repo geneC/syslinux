72fd5d37e68617c3bdd7aff56f519a1e5c9d8817
H. Peter Anvin <hpa@zytor.com>
2006-08-28 02:46:06 -0700

    Test program for the VESA code

---
+++ com32/samples/vesa.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "../lib/sys/vesa/video.h"
+
+int __vesacon_init(void);
+void vesacon_write_at(int row, int col, const char *str, uint8_t attr, int rev);
+
+int main(void)
+{
+  int row, col;
+  int attr;
+  char attr_buf[16];
+
+  __vesacon_init();
+
+  row = col = 0;
+  
+  for (attr = 0; attr < 256; attr++) {
+    snprintf(attr_buf, sizeof attr_buf, " [%02X] ", attr);
+    vesacon_write_at(row, col, attr_buf, attr, attr & 3);
+    row++;
+    if (row >= 29) {
+      row = 0;
+      col += 8;
+    }
+  }  
+
+  while (1)
+    asm volatile("hlt");
+}
