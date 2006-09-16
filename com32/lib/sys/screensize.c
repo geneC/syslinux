e1513d5d07e0b78316414731c550d3248d09bd19
H. Peter Anvin <hpa@zytor.com>
2006-09-16 16:29:08 -0700

    Add file missing from previous commit

---
+++ com32/lib/sys/screensize.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <errno.h>
+#include "file.h"
+
+int getscreensize(int fd, int *rows, int *cols)
+{
+  struct file_info *fp = &__file_info[fd];
+
+  if ( fd >= NFILES || !fp->iop ) {
+    errno = EBADF;
+    return -1;
+  }
+
+  *rows = fp->o.rows;
+  *cols = fp->o.cols;
+
+  if (!rows || !cols) {
+    errno = ENOTTY;
+    return -1;
+  }
+
+  return 0;
+}
