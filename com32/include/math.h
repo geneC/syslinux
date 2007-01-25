5ed6636a950d39bc2fec9b3485f229eb7f0852e1
H. Peter Anvin <hpa@zytor.com>
2007-01-25 13:58:29 -0800

    Compilation fix for older gcc where __DBL_*_EXP__ isn't defined

--- com32/include/math.h
+++ com32/include/math.h
@@ -1,6 +1,13 @@
 #ifndef _MATH_H
 #define _MATH_H
 
+#ifndef __DBL_MIN_EXP__
+# define __DBL_MIN_EXP__ (-1021)
+#endif
+#ifndef __DBL_MAX_EXP__
+# define __DBL_MAX_EXP__ 1024
+#endif
+
 double pow(double, double);
 double fabs(double);
 double strtod(const char *, char **);
