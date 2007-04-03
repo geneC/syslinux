0b3b862ff49ab1b0b5d5af8c15d58e3c4baaf634
H. Peter Anvin <hpa@zytor.com>
2007-04-03 15:07:31 -0700

    A few more syslinux wrapper function
    Add implementations of a few more syslinux wrapper functions:
    
    syslinux_run_command()
    syslinux_run_default()
    syslinux_final_cleanup()

---
+++ com32/lib/syslinux/run_command.c
@@ -0,0 +1,47 @@
+/* ----------------------------------------------------------------------- *
+ *
+ *   Copyright 2007 H. Peter Anvin - All Rights Reserved
+ *
+ *   Permission is hereby granted, free of charge, to any person
+ *   obtaining a copy of this software and associated documentation
+ *   files (the "Software"), to deal in the Software without
+ *   restriction, including without limitation the rights to use,
+ *   copy, modify, merge, publish, distribute, sublicense, and/or
+ *   sell copies of the Software, and to permit persons to whom
+ *   the Software is furnished to do so, subject to the following
+ *   conditions:
+ *
+ *   The above copyright notice and this permission notice shall
+ *   be included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *   OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * ----------------------------------------------------------------------- */
+
+#include <syslinux/boot.h>
+#include <stddef.h>
+#include <string.h>
+#include <com32.h>
+
+__noreturn syslinux_run_command(const char *command)
+{
+  static com32sys_t ireg;
+
+  strcpy(__com32.cs_bounce, command);
+
+  ireg.eax.w[0] = 0x0003;
+  ireg.es = SEG(__com32.cs_bounce);
+  ireg.ebx.w[0] = OFFS(__com32.cs_bounce);
+
+  __intcall(0x22, &ireg, NULL);
+
+  /* Should not return even on failure */
+  for(;;);
+}
