5bcb0644ba86675c5912714d8b91689c0859b4ca
H. Peter Anvin <hpa@zytor.com>
2007-08-14 15:20:13 -0700

    Add reboot.c32 module
    A very simple module to reboot the machine.

---
+++ com32/modules/reboot.c
@@ -0,0 +1,16 @@
+#include <syslinux/reboot.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+  int warm = 0;
+  int i;
+
+  for (i = 1; i < argc; i++) {
+    if (!strcmp(argv[i], "-w") ||
+	!strcmp(argv[i], "--warm"))
+      warm = 1;
+  }
+
+  syslinux_reboot(warm);
+}
