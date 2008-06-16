52ff420fba565c4803cd43260c2d36189ff3d71a
H. Peter Anvin <hpa@zytor.com>
2008-06-16 00:07:23 -0700

    codepage: include case variant characters in UnicodeData
    Adjust the gensubset.pl script to that all case variants are
    explicitly included in UnicodeData.

--- codepage/gensubset.pl
+++ codepage/gensubset.pl
@@ -9,6 +9,7 @@
 
 %need_these = ();
 
+# Mark as needed all the characters mentioned in the relevant files
 foreach $file (@ARGV) {
     open(F, '<', $file) or die;
     while (defined($line = <F>)) {
@@ -20,9 +21,27 @@ foreach $file (@ARGV) {
     close(F);
 }
 
+# Also mark as needed any case variants of those
+# (Note: this doesn't necessarily provide the full transitive closure,
+# but we shouldn't need it.)
+while (defined($line = <STDIN>)) {
+    @f = split(/;/, $line);
+    if ($f[0] =~ /^([0-9a-f]+)$/i) {
+	$r = hex $f[0];
+	if ($need_these{$r}) {
+	    $need_these{hex $f[12]}++ if ($f[12] ne '');
+	    $need_these{hex $f[13]}++ if ($f[13] ne '');
+	    $need_these{hex $f[14]}++ if ($f[14] ne '');
+	}
+    }
+}
+
+# Finally, write out the subset
+seek(STDIN, 0, 0);
 while (defined($line = <STDIN>)) {
     ($v, $l) = split(/;/, $line, 2);
     if ($v =~ /^([0-9a-f]+)\-([0-9a-f]+)$/i) {
+	# This isn't actually the format... fix that if it ever matters
 	$r1 = hex $1;
 	$r2 = hex $2;
     } elsif ($v =~ /^([0-9a-f]+)$/i) {
