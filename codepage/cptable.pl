385e460b5a9738eda6a4344012bad09b431a4ee0
H. Peter Anvin <hpa@zytor.com>
2008-06-25 12:10:44 -0700

    Add more codepage data; support disjoint mappings
    Add additional codepage data from the Unicode Consortium.
    Additionally, add support for generating codepage files for the case
    where the filesystem codepage and the display codepage do not match.
    The standard Makefile doesn't actually do that, however.

--- codepage/cptable.pl
+++ codepage/cptable.pl
@@ -5,27 +5,28 @@
 # matching.)
 #
 # Usage:
-#	cptable.pl UnicodeData cpXXX.txt cpXXX.bin
+#	cptable.pl UnicodeData filesystem-cp.txt console-cp.txt output.bin
 #
 # Note: for the format of the UnicodeData file, see:
 # http://www.unicode.org/Public/UNIDATA/UCD.html
 #
 
-($ucd, $cpin, $cpout) = @ARGV;
+($ucd, $cpfs, $cpco, $cpout) = @ARGV;
 
 %ucase   = ();
 %lcase   = ();
 %tcase   = ();
 %decomp  = ();
 
-open(UCD, '<', $ucd) or die;
+open(UCD, '<', $ucd)
+    or die "$0: could not open unicode data: $ucd: $!\n";
 while (defined($line = <UCD>)) {
     chomp $line;
     @f = split(/;/, $line);
     $n = hex $f[0];
-    $ucase{$n} = hex $f[12] if ($f[12] ne '');
-    $lcase{$n} = hex $f[13] if ($f[13] ne '');
-    $tcase{$n} = hex $f[14] if ($f[14] ne '');
+    $ucase{$n} = ($f[12] ne '') ? hex $f[12] : $n;
+    $lcase{$n} = ($f[13] ne '') ? hex $f[13] : $n;
+    $tcase{$n} = ($f[14] ne '') ? hex $f[14] : $n;
     if ($f[5] =~ /^[0-9A-F\s]+$/) {
 	# This character has a canonical decomposition.
 	# The regular expression rejects angle brackets, so other
@@ -38,11 +39,16 @@ while (defined($line = <UCD>)) {
 }
 close(UCD);
 
+#
+# Filesystem and console codepages.  The filesystem codepage is used
+# for FAT shortnames, whereas the console codepage is whatever is used
+# on the screen and keyboard.
+#
 @xtab = (undef) x 256;
 %tabx = ();
-
-open(CPIN, '<', $cpin) or die;
-while (defined($line = <CPIN>)) {
+open(CPFS, '<', $cpfs)
+    or die "$0: could not open fs codepage: $cpfs: $!\n";
+while (defined($line = <CPFS>)) {
     $line =~ s/\s*(\#.*|)$//;
     @f = split(/\s+/, $line);
     next if (scalar @f != 2);
@@ -50,9 +56,24 @@ while (defined($line = <CPIN>)) {
     $xtab[hex $f[0]] = hex $f[1]; # Codepage -> Unicode
     $tabx{hex $f[1]} = hex $f[0]; # Unicode -> Codepage
 }
-close(CPIN);
+close(CPFS);
 
-open(CPOUT, '>', $cpout) or die;
+@ytab = (undef) x 256;
+%taby = ();
+open(CPCO, '<', $cpco)
+    or die "$0: could not open console codepage: $cpco: $!\n";
+while (defined($line = <CPCO>)) {
+    $line =~ s/\s*(\#.*|)$//;
+    @f = split(/\s+/, $line);
+    next if (scalar @f != 2);
+    next if (hex $f[0] > 255);
+    $ytab[hex $f[0]] = hex $f[1]; # Codepage -> Unicode
+    $taby{hex $f[1]} = hex $f[0]; # Unicode -> Codepage
+}
+close(CPCO);
+
+open(CPOUT, '>', $cpout)
+    or die "$0: could not open output file: $cpout: $!\n";
 #
 # Magic number, in anticipation of being able to load these
 # files dynamically...
@@ -63,10 +84,17 @@ print CPOUT pack("VV", 0x8fad232b, 0x9c295319);
 print CPOUT pack("VVVVVV", 0, 0, 0, 0, 0, 0);
 
 #
-# Self (shortname) uppercase table
+# Self (shortname) uppercase table.
+# This depends both on the console codepage and the filesystem codepage;
+# the logical transcoding operation is:
+#
+# $tabx{$ucase{$ytab[$i]}}
+#
+# ... where @ytab is console codepage -> Unicode and
+# %tabx is Unicode -> filesystem codepage.
 #
 for ($i = 0; $i < 256; $i++) {
-    $uuc = $ucase{$xtab[$i]};	# Unicode upper case
+    $uuc = $ucase{$ytab[$i]};	# Unicode upper case
     if (defined($tabx{$uuc})) {
 	# Straight-forward conversion
 	$u = $tabx{$uuc};
@@ -74,24 +102,28 @@ for ($i = 0; $i < 256; $i++) {
 	# Upper case equivalent stripped of accents
 	$u = $tabx{${$decomp{$uuc}}[0]};
     } else {
-	$u = $i;
+	# No equivalent at all found.  Set this to zero, which should
+	# prevent shortname matching altogether (still making longname
+	# matching possible, of course.)
+	$u = 0;
     }
     print CPOUT pack("C", $u);
 }
 
 #
-# Unicode (longname) matching table
+# Unicode (longname) matching table.
+# This only depends on the console codepage.
 #
 for ($i = 0; $i < 256; $i++) {
-    if (!defined($xtab[$i])) {
+    if (!defined($ytab[$i])) {
 	$p0 = $p1 = 0xffff;
     } else {
-	$p0 = $xtab[$i];
-	if (defined($ucase{$p0})) {
+	$p0 = $ytab[$i];
+	if ($ucase{$p0} != $p0) {
 	    $p1 = $ucase{$p0};
-	} elsif (defined($lcase{$p0})) {
+	} elsif ($lcase{$p0} != $p0) {
 	    $p1 = $lcase{$p0};
-	} elsif (defined($tcase{$p0})) {
+	} elsif ($tcase{$p0} != $p0) {
 	    $p1 = $tcase{$p0};
 	} else {
 	    $p1 = $p0;
