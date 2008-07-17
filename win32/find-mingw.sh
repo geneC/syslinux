963a38fc45a50d555d61f4a4640b02d7c398b4f4
H. Peter Anvin <hpa@zytor.com>
2008-07-16 17:01:33 -0700

    win32: search for a mingw compiler under several names
    Search for the MinGW compiler under several names, since different
    distros like to install it under different names.  What's wrong with a
    simple mingw- prefix??
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

---
+++ win32/find-mingw.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cc="$1"
+
+for prefix in \
+    mingw- \
+    i386-mingw32- \
+    i486-mingw32- \
+    i586-mingw32- \
+    i686-mingw32- \
+    i386-mingw32msvc- \
+    i486-mingw32msvc- \
+    i586-mingw32msvc- \
+    i686-mingw32msvc-; do
+    if "${prefix}${cc}" -v > /dev/null 2>&1; then
+	echo "$prefix"
+	exit 0
+    fi
+done
+
+# No prefix, no idea what to do now...
+echo missing-
+exit 1
