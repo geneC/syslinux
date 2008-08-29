77950e64a458e753900ff4ff6119bea15d1a4533
Stefan Hajnoczi <stefanha@gmail.com>
2008-08-29 15:08:26 +0100

    Add GDB stub

--- com32/gdbstub/main.c
+++ com32/gdbstub/main.c
@@ -4,13 +4,42 @@
 #include <console.h>
 #include <com32.h>
 #include <syslinux/loadfile.h>
+#include "serial.h"
 
+#define X86_INT_DB      1
+#define X86_INT_BP      3
+#define COM32_IDT       ((void*)0x100000)
 #define COM32_LOAD_ADDR ((void*)0x101000)
-#define STACK_RED_ZONE  0x1000
+#define STACK_SIZE      0x1000
+
+extern char _start[], _end[];
+
+struct reloc_info {
+    void *data;
+    size_t len;
+    uint32_t old_esp;
+    uint32_t reloc_base;
+};
 
 static inline void error(const char *msg)
 {
     fputs(msg, stderr);
+}
+
+static inline uint32_t reloc_ptr(struct reloc_info *ri, void *ptr)
+{
+    return ri->reloc_base + (uint32_t)((char*)ptr - _start);
+}
+
+static void hijack_interrupt(int intn, uint32_t handler)
+{
+    struct {
+        uint32_t lo;
+        uint32_t hi;
+    } *idt = COM32_IDT;
+
+    idt[intn].lo = (idt[intn].lo & 0xffff0000) | (handler & 0x0000ffff);
+    idt[intn].hi = (idt[intn].hi & 0x0000ffff) | (handler & 0xffff0000);
 }
 
 static void shift_cmdline(struct com32_sys_args *com32)
@@ -32,52 +61,67 @@ static void shift_cmdline(struct com32_sys_args *com32)
     com32->cs_cmdline = p;
 }
 
-static __noreturn reloc_entry(void *ptr, size_t len, uintptr_t entry_esp, uintptr_t module_esp)
+static __noreturn reloc_entry(struct reloc_info *ri)
 {
+    extern char int_handler[];
     size_t stack_frame_size = sizeof(struct com32_sys_args) + 4;
     struct com32_sys_args *com32;
+    uint32_t module_esp;
+
+    hijack_interrupt(X86_INT_DB, reloc_ptr(ri, int_handler));
+    hijack_interrupt(X86_INT_BP, reloc_ptr(ri, int_handler));
 
     /* Copy module to load address */
-    memcpy(COM32_LOAD_ADDR, ptr, len);
+    memcpy(COM32_LOAD_ADDR, ri->data, ri->len);
 
     /* Copy stack frame onto module stack */
-    module_esp = (module_esp - stack_frame_size) & ~15;
-    memcpy((void*)module_esp, (void*)entry_esp, stack_frame_size);
+    module_esp = (ri->reloc_base - stack_frame_size) & ~15;
+    memcpy((void*)module_esp, (void*)ri->old_esp, stack_frame_size);
 
     /* Fix up command line */
-    com32 = (struct com32_sys_args*)((char*)module_esp + 4);
+    com32 = (struct com32_sys_args*)(module_esp + 4);
     shift_cmdline(com32);
 
-    /* Invoke module with stack set up */
+    /* Set up CPU state to run module and enter GDB */
     asm volatile (
             "movl %0, %%esp\n\t"
-            "jmp *%%ecx"
-            : : "r"(module_esp), "c"(COM32_LOAD_ADDR)
+            "pushf\n\t"
+            "pushl %%cs\n\t"
+            "pushl %1\n\t"
+            "jmp *%2\n\t"
+            : : "r"(module_esp),
+                "c"(COM32_LOAD_ADDR),
+                "r"(reloc_ptr(ri, int_handler))
     );
     for(;;); /* shut the compiler up */
 }
 
 static inline __noreturn reloc(void *ptr, size_t len)
 {
-    extern uintptr_t __entry_esp;
-    extern char _start[], _end[];
+    extern uint32_t __entry_esp;
     size_t total_size = _end - _start;
-    __noreturn (*success_fn)(void*, size_t, uintptr_t, uintptr_t);
+    __noreturn (*entry_fn)(struct reloc_info*);
+    struct reloc_info ri;
     uint32_t esp;
     char *dest;
 
     /* Calculate relocation address, preserve current stack */
     asm volatile ("movl %%esp, %0\n\t" : "=m"(esp));
-    dest = (char*)((esp - STACK_RED_ZONE - total_size) & ~3);
+    dest = (char*)((esp - STACK_SIZE - total_size) & ~3);
 
     /* Calculate entry point in relocated code */
-    success_fn = (void*)(dest + ((char*)reloc_entry - _start));
+    entry_fn = (void*)(dest + ((char*)reloc_entry - _start));
 
     /* Copy all sections to relocation address */
     printf("Relocating %d bytes from %p to %p\n", total_size, _start, dest);
     memcpy(dest, _start, total_size);
 
-    success_fn(ptr, len, __entry_esp, (uintptr_t)dest);
+    /* Call into relocated code */
+    ri.data = ptr;
+    ri.len = len;
+    ri.old_esp = __entry_esp;
+    ri.reloc_base = (uint32_t)dest;
+    entry_fn(&ri);
 }
 
 int main(int argc, char *argv[])
@@ -96,6 +140,8 @@ int main(int argc, char *argv[])
         error("Unable to load file\n");
         return 1;
     }
+
+    serial_init();
 
     /* No more lib calls after this point */
     reloc(data, data_len);
---
+++ com32/gdbstub/serial.c
@@ -0,0 +1,190 @@
+/*
+ * The serial port interface routines implement a simple polled i/o
+ * interface to a standard serial port.  Due to the space restrictions
+ * for the boot blocks, no BIOS support is used (since BIOS requires
+ * expensive real/protected mode switches), instead the rudimentary
+ * BIOS support is duplicated here.
+ *
+ * The base address and speed for the i/o port are passed from the
+ * Makefile in the COMCONSOLE and CONSPEED preprocessor macros.  The
+ * line control parameters are currently hard-coded to 8 bits, no
+ * parity, 1 stop bit (8N1).  This can be changed in init_serial().
+ */
+
+#include <stddef.h>
+#include <sys/io.h>
+#include "serial.h"
+
+/* Set default values if none specified */
+
+#ifndef COMCONSOLE
+#define COMCONSOLE	0x3f8
+#endif
+
+#ifndef COMSPEED
+#define COMSPEED	9600
+#endif
+
+#ifndef COMDATA
+#define COMDATA		8
+#endif
+
+#ifndef COMPARITY
+#define COMPARITY	0
+#endif
+
+#ifndef COMSTOP
+#define COMSTOP		1
+#endif
+
+#undef UART_BASE
+#define UART_BASE ( COMCONSOLE )
+
+#undef UART_BAUD
+#define UART_BAUD ( COMSPEED )
+
+#if ((115200%UART_BAUD) != 0)
+#error Bad ttys0 baud rate
+#endif
+
+#define COMBRD (115200/UART_BAUD)
+
+/* Line Control Settings */
+#define UART_LCS ( ( ( (COMDATA) - 5 )	<< 0 ) | \
+		   ( ( (COMPARITY) )	<< 3 ) | \
+		   ( ( (COMSTOP) - 1 )	<< 2 ) )
+
+/* Data */
+#define UART_RBR 0x00
+#define UART_TBR 0x00
+
+/* Control */
+#define UART_IER 0x01
+#define UART_IIR 0x02
+#define UART_FCR 0x02
+#define UART_LCR 0x03
+#define UART_MCR 0x04
+#define UART_DLL 0x00
+#define UART_DLM 0x01
+
+/* Status */
+#define UART_LSR 0x05
+#define  UART_LSR_TEMPT 0x40	/* Transmitter empty */
+#define  UART_LSR_THRE  0x20	/* Transmit-hold-register empty */
+#define  UART_LSR_BI	0x10	/* Break interrupt indicator */
+#define  UART_LSR_FE	0x08	/* Frame error indicator */
+#define  UART_LSR_PE	0x04	/* Parity error indicator */
+#define  UART_LSR_OE	0x02	/* Overrun error indicator */
+#define  UART_LSR_DR	0x01	/* Receiver data ready */
+
+#define UART_MSR 0x06
+#define UART_SCR 0x07
+
+#define uart_readb(addr) inb(addr)
+#define uart_writeb(val,addr) outb((val),(addr))
+
+/*
+ * void serial_putc(int ch);
+ *	Write character `ch' to port UART_BASE.
+ */
+void serial_putc ( int ch ) {
+	int status;
+	for (;;) {
+		status = uart_readb(UART_BASE + UART_LSR);
+		if (status & UART_LSR_THRE) { 
+			/* TX buffer emtpy */
+			uart_writeb(ch, UART_BASE + UART_TBR);
+			break;
+		}
+	}
+}
+
+/*
+ * int serial_getc(void);
+ *	Read a character from port UART_BASE.
+ */
+int serial_getc ( void ) {
+	int status;
+	int ch;
+	do {
+		status = uart_readb(UART_BASE + UART_LSR);
+	} while((status & 1) == 0);
+	ch = uart_readb(UART_BASE + UART_RBR);	/* fetch (first) character */
+	ch &= 0x7f;				/* remove any parity bits we get */
+	if (ch == 0x7f) {			/* Make DEL... look like BS */
+		ch = 0x08;
+	}
+	return ch;
+}
+
+/*
+ * int serial_init(void);
+ *	Initialize port UART_BASE to speed COMSPEED, line settings 8N1.
+ */
+void serial_init ( void ) {
+	int status;
+	int divisor, lcs;
+
+	divisor = COMBRD;
+	lcs = UART_LCS;
+
+
+#ifdef COMPRESERVE
+	lcs = uart_readb(UART_BASE + UART_LCR) & 0x7f;
+	uart_writeb(0x80 | lcs, UART_BASE + UART_LCR);
+	divisor = (uart_readb(UART_BASE + UART_DLM) << 8) | uart_readb(UART_BASE + UART_DLL);
+	uart_writeb(lcs, UART_BASE + UART_LCR);
+#endif
+
+	/* Set Baud Rate Divisor to COMSPEED, and test to see if the
+	 * serial port appears to be present.
+	 */
+	uart_writeb(0x80 | lcs, UART_BASE + UART_LCR);
+	uart_writeb(0xaa, UART_BASE + UART_DLL);
+	if (uart_readb(UART_BASE + UART_DLL) != 0xaa) {
+		goto out;
+	}
+	uart_writeb(0x55, UART_BASE + UART_DLL);
+	if (uart_readb(UART_BASE + UART_DLL) != 0x55) {
+		goto out;
+	}
+	uart_writeb(divisor & 0xff, UART_BASE + UART_DLL);
+	if (uart_readb(UART_BASE + UART_DLL) != (divisor & 0xff)) {
+		goto out;
+	}
+	uart_writeb(0xaa, UART_BASE + UART_DLM);
+	if (uart_readb(UART_BASE + UART_DLM) != 0xaa) {
+		goto out;
+	}
+	uart_writeb(0x55, UART_BASE + UART_DLM);
+	if (uart_readb(UART_BASE + UART_DLM) != 0x55) {
+		goto out;
+	}
+	uart_writeb((divisor >> 8) & 0xff, UART_BASE + UART_DLM);
+	if (uart_readb(UART_BASE + UART_DLM) != ((divisor >> 8) & 0xff)) {
+		goto out;
+	}
+	uart_writeb(lcs, UART_BASE + UART_LCR);
+	
+	/* disable interrupts */
+	uart_writeb(0x0, UART_BASE + UART_IER);
+
+	/* disable fifo's */
+	uart_writeb(0x00, UART_BASE + UART_FCR);
+
+	/* Set clear to send, so flow control works... */
+	uart_writeb((1<<1), UART_BASE + UART_MCR);
+
+
+	/* Flush the input buffer. */
+	do {
+		/* rx buffer reg
+		 * throw away (unconditionally the first time)
+		 */
+	        (void) uart_readb(UART_BASE + UART_RBR);
+		/* line status reg */
+		status = uart_readb(UART_BASE + UART_LSR);
+	} while(status & UART_LSR_DR);
+ out:
+	return;
+}
