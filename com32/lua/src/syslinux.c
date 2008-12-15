16fa94bd8c1556d7aae3942293cba0a2f61e4ce7
Marcel Ritter <unrzl1@linux.rrze.uni-erlangen.de>
2008-12-15 08:35:55 +0100

    COM32: lua - added simple syslinux_run_command function

---
+++ com32/lua/src/syslinux.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+
+#define lnetlib_c       /* Define the library */
+
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+
+static int sl_run_command(lua_State *L)
+{
+  const char *cmd = luaL_checkstring(L, 1);  /* Reads the string parameter */
+  syslinux_run_command(cmd);
+  return 0;
+}
+
+
+static const luaL_reg syslinuxlib[] = {
+  {"run_command", sl_run_command},
+  {NULL, NULL}
+};
+
+/* This defines a function that opens up your library. */
+
+LUALIB_API int luaopen_syslinux (lua_State *L) {
+  luaL_openlib(L, LUA_SYSLINUXLIBNAME, syslinuxlib, 0);
+  return 1;
+}
+
