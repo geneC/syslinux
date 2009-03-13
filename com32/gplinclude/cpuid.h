d617310bda3b892fb8626151891748c1ee98106d
H. Peter Anvin <hpa@zytor.com>
2009-03-12 21:14:36 -0700

    Update the cpuid.h license header

--- com32/gplinclude/cpuid.h
+++ com32/gplinclude/cpuid.h
@@ -1,20 +1,17 @@
-/*
- * Portions of this file taken from the Linux kernel,
- * Copyright 1991-2009 Linus Torvalds and contributors
+/* ----------------------------------------------------------------------- *
+ *   
+ *   Copyright 2006-2009 Erwan Velu - All Rights Reserved
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *   Portions of this file taken from the Linux kernel,
+ *   Copyright 1991-2009 Linus Torvalds and contributors
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2
+ *   as published by the Free Software Foundation, Inc.,
+ *   51 Franklin St, Fifth Floor, Boston MA 02110-1301;
+ *   incorporated herein by reference.
  *
-*/
+ * ----------------------------------------------------------------------- */
 
 #ifndef _CPUID_H
 #define _CPUID_H
