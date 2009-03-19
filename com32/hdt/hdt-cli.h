1d07797ed789c35429fd7702180e86be506069b3
Erwan Velu <erwan.velu@free.fr>
2009-03-19 11:34:49 +0100

    hdt: Rewrite DMI module using a new syntax for the CLI
    Impact: Start refactoring the CLI.
    
    In order to have an extensible CLI, the commands handling needs to be
    refactored. This patch starts the migration by refactoring the DMI
    module.
    
    Note that the core of the new commands parser has been implemented.
    Adapting other modules to use it simply requires to declare
    new structures that maps the command string to the callback.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- com32/hdt/hdt-cli.h
+++ com32/hdt/hdt-cli.h
@@ -71,6 +71,26 @@ struct s_cli_mode {
   char prompt[32];
 };
 
+/* A command-line command */
+struct commands_mode {
+  const unsigned int mode;
+  struct commands_module_descr* show_modules;
+  /* Future: set? */
+};
+
+struct commands_module {
+  const char *name;
+  void ( * exec ) ( int argc, char** argv, struct s_hardware *hardware );
+};
+
+/* Describe 'show', 'set', ... commands in a module */
+struct commands_module_descr {
+  struct commands_module* modules;
+  const int nb_modules;
+};
+
+struct commands_mode dmi_mode;
+
 void start_cli_mode(struct s_hardware *hardware);
 void main_show(char *item, struct s_hardware *hardware);
 int do_exit(struct s_cli_mode *cli_mode);
@@ -84,10 +104,11 @@ int do_exit(struct s_cli_mode *cli_mode);
 #define CLI_DMI_MEMORY_BANK "bank"
 #define CLI_DMI_PROCESSOR "cpu"
 #define CLI_DMI_SYSTEM "system"
+#define CLI_DMI_LIST CLI_SHOW_LIST
+#define CLI_DMI_MAX_MODULES 9
 void main_show_dmi(struct s_hardware *hardware);
 void handle_dmi_commands(char *cli_line, struct s_hardware *hardware);
-void show_dmi_memory_modules(struct s_hardware *hardware, bool clearscreen,
-                             bool show_free_banks);
+void show_dmi_memory_modules(int argc, char** argv, struct s_hardware *hardware);
 
 // PCI STUFF
 #define CLI_PCI_DEVICE "device"
