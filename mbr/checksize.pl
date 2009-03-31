848c8f9a4133e4e0113120a2b311d2352af26228
H. Peter Anvin <hpa@linux.intel.com>
2009-03-30 18:17:05 -0700

    altmbr: an alternative MBR which ignores the active flag
    Impact: new feature
    
    Create an alternative MBR which takes a partition number in byte 439
    instead of looking at the active flag.  This is useful when
    dual-booting legacy operating systems which assume that their
    particular partition must be the active partition.
    
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- mbr/checksize.pl
+++ mbr/checksize.pl
@@ -1,6 +1,6 @@
 ## -----------------------------------------------------------------------
 ##
-##   Copyright 2007-2008 H. Peter Anvin - All Rights Reserved
+##   Copyright 2007-2009 H. Peter Anvin - All Rights Reserved
 ##
 ##   This program is free software; you can redistribute it and/or modify
 ##   it under the terms of the GNU General Public License as published by
@@ -13,18 +13,26 @@
 ##
 ## checksize.pl
 ##
-## Check the size of a binary file
+## Check the size of a binary file and pad it with zeroes to that size
 ##
 
-($file, $maxsize) = @ARGV;
+use bytes;
 
-@st = stat($file);
+($file, $maxsize, $padsize) = @ARGV;
+
+$padsize = $maxsize unless(defined($padsize));
+
+open(FILE, '+<', $file) or die;
+@st = stat(FILE);
 if (!defined($size = $st[7])) {
     die "$0: $file: $!\n";
 }
 if ($size > $maxsize) {
     print STDERR "$file: too big ($size > $maxsize)\n";
     exit 1;
-} else {
-    exit 0;
+} elsif ($size < $padsize) {
+    seek(FILE, $size, 0);
+    print FILE "\0" x ($padsize-$size);
 }
+
+exit 0;
