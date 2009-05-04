8029ff89d33361edf48081748ff9db1239897694
Sebastian Herbszt <herbszt@gmx.de>
2009-05-04 23:42:24 +0200

    poweroff: add copyright notice
    Add a copyright notice.
    
    Signed-off-by: Sebastian Herbszt <herbszt@gmx.de>
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- modules/poweroff.asm
+++ modules/poweroff.asm
@@ -1,3 +1,19 @@
+; ****************************************************************************
+;
+;  poweroff.asm
+;
+;   Copyright 2009 Sebastian Herbszt
+;
+;  APM poweroff module.
+;
+;  This program is free software; you can redistribute it and/or modify
+;  it under the terms of the GNU General Public License as published by
+;  the Free Software Foundation, Inc., 53 Temple Place Ste 330,
+;  Boston MA 02111-1307, USA; either version 2 of the License, or
+;  (at your option) any later version; incorporated herein by reference.
+;
+; ****************************************************************************
+
 		absolute 0
 pspInt20:	resw 1
 pspNextP:	resw 1
