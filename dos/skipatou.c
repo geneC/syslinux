6dc5c514c344f88b538de28a501945f3cee541ca
H. Peter Anvin <hpa@zytor.com>
2009-05-29 15:10:32 -0700

    Run Nindent on dos/skipatou.c
    Automatically reformat dos/skipatou.c using Nindent.
    
    Do this for all files except HDT, gPXE and externally maintained
    libraries (zlib, tinyjpeg, libpng).
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- dos/skipatou.c
+++ dos/skipatou.c
@@ -2,9 +2,9 @@
 
 unsigned int skip_atou(const char **s)
 {
-  int i=0;
+    int i = 0;
 
-  while (isdigit(**s))
-    i = i*10 + *((*s)++) - '0';
-  return i;
+    while (isdigit(**s))
+	i = i * 10 + *((*s)++) - '0';
+    return i;
 }
