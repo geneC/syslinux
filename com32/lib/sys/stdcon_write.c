00b01b81285656c8aec91d6490d736f340b7505d
H. Peter Anvin <hpa@zytor.com>
2009-05-29 15:10:25 -0700

    Run Nindent on com32/lib/sys/stdcon_write.c
    Automatically reformat com32/lib/sys/stdcon_write.c using Nindent.
    
    Do this for all files except HDT, gPXE and externally maintained
    libraries (zlib, tinyjpeg, libpng).
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- com32/lib/sys/stdcon_write.c
+++ com32/lib/sys/stdcon_write.c
@@ -37,35 +37,36 @@
 #include <minmax.h>
 #include "file.h"
 
-static ssize_t __stdcon_write(struct file_info *fp, const void *buf, size_t count)
+static ssize_t __stdcon_write(struct file_info *fp, const void *buf,
+			      size_t count)
 {
-  com32sys_t ireg;
-  const char *bufp = buf;
-  size_t n = 0;
+    com32sys_t ireg;
+    const char *bufp = buf;
+    size_t n = 0;
 
-  (void)fp;
+    (void)fp;
 
-  memset(&ireg, 0, sizeof ireg);
-  ireg.eax.b[1] = 0x02;
+    memset(&ireg, 0, sizeof ireg);
+    ireg.eax.b[1] = 0x02;
 
-  while ( count-- ) {
-    if ( *bufp == '\n' ) {
-      ireg.edx.b[0] = '\r';
-      __intcall(0x21, &ireg, NULL);
+    while (count--) {
+	if (*bufp == '\n') {
+	    ireg.edx.b[0] = '\r';
+	    __intcall(0x21, &ireg, NULL);
+	}
+	ireg.edx.b[0] = *bufp++;
+	__intcall(0x21, &ireg, NULL);
+	n++;
     }
-    ireg.edx.b[0] = *bufp++;
-    __intcall(0x21, &ireg, NULL);
-    n++;
-  }
 
-  return n;
+    return n;
 }
 
 const struct output_dev dev_stdcon_w = {
-  .dev_magic  = __DEV_MAGIC,
-  .flags      = __DEV_TTY | __DEV_OUTPUT,
-  .fileflags  = O_WRONLY | O_CREAT | O_TRUNC | O_APPEND,
-  .write      = __stdcon_write,
-  .close      = NULL,
-  .open       = NULL,
+    .dev_magic = __DEV_MAGIC,
+    .flags = __DEV_TTY | __DEV_OUTPUT,
+    .fileflags = O_WRONLY | O_CREAT | O_TRUNC | O_APPEND,
+    .write = __stdcon_write,
+    .close = NULL,
+    .open = NULL,
 };
