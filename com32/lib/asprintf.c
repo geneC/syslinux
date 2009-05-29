b94a2a040107271754c190376aa132ecf0f6bab6
H. Peter Anvin <hpa@zytor.com>
2009-05-29 15:10:22 -0700

    Run Nindent on com32/lib/asprintf.c
    Automatically reformat com32/lib/asprintf.c using Nindent.
    
    Do this for all files except HDT, gPXE and externally maintained
    libraries (zlib, tinyjpeg, libpng).
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- com32/lib/asprintf.c
+++ com32/lib/asprintf.c
@@ -8,23 +8,23 @@
 
 int asprintf(char **bufp, const char *format, ...)
 {
-  va_list ap, ap1;
-  int rv;
-  int bytes;
-  char *p;
+    va_list ap, ap1;
+    int rv;
+    int bytes;
+    char *p;
 
-  va_start(ap, format);
-  va_copy(ap1, ap);
+    va_start(ap, format);
+    va_copy(ap1, ap);
 
-  bytes = vsnprintf(NULL, 0, format, ap1) + 1;
-  va_end(ap1);
+    bytes = vsnprintf(NULL, 0, format, ap1) + 1;
+    va_end(ap1);
 
-  *bufp = p = malloc(bytes);
-  if ( !p )
-    return -1;
+    *bufp = p = malloc(bytes);
+    if (!p)
+	return -1;
 
-  rv = vsnprintf(p, bytes, format, ap);
-  va_end(ap);
+    rv = vsnprintf(p, bytes, format, ap);
+    va_end(ap);
 
-  return rv;
+    return rv;
 }
