2e63f522953e48017d55e50aacc8c5e0754e1284
H. Peter Anvin <hpa@zytor.com>
2009-05-29 15:20:14 -0700

    Run Nindent on core/hello.c
    Automatically reformat core/hello.c using Nindent.
    
    Do this for all files except HDT, gPXE and externally maintained
    libraries (zlib, tinyjpeg, libpng).
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- core/hello.c
+++ core/hello.c
@@ -3,25 +3,25 @@
 
 void myputchar(int c)
 {
-  static com32sys_t ireg;
-  static uint16_t *vram = 0xb8000;
+    static com32sys_t ireg;
+    static uint16_t *vram = 0xb8000;
 
-  ireg.eax.b[1] = 0x02;
-  ireg.edx.b[0] = c;
-  core_intcall(0x21, &ireg, NULL);
+    ireg.eax.b[1] = 0x02;
+    ireg.edx.b[0] = c;
+    core_intcall(0x21, &ireg, NULL);
 
-  *vram++ = c + 0x1f00;
+    *vram++ = c + 0x1f00;
 }
 
 void myputs(const char *str)
 {
-  while (*str)
-    myputchar(*str++);
+    while (*str)
+	myputchar(*str++);
 }
 
 void hello(void)
 {
-  static char hello_str[] = "Hello, World!  (hello.c)\r\n";
+    static char hello_str[] = "Hello, World!  (hello.c)\r\n";
 
-  myputs(hello_str);
+    myputs(hello_str);
 }
