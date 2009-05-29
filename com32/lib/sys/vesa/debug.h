0cc7d185e6fcbda902d29b95ba49eaa32e8df8cf
H. Peter Anvin <hpa@zytor.com>
2009-05-29 15:10:25 -0700

    Run Nindent on com32/lib/sys/vesa/debug.h
    Automatically reformat com32/lib/sys/vesa/debug.h using Nindent.
    
    Do this for all files except HDT, gPXE and externally maintained
    libraries (zlib, tinyjpeg, libpng).
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- com32/lib/sys/vesa/debug.h
+++ com32/lib/sys/vesa/debug.h
@@ -10,25 +10,25 @@ ssize_t __serial_write(void *fp, const void *buf, size_t count);
 
 static void debug(const char *str, ...)
 {
-  va_list va;
-  char buf[65536];
-  size_t len;
+    va_list va;
+    char buf[65536];
+    size_t len;
 
-  va_start(va, str);
-  len = vsnprintf(buf, sizeof buf, str, va);
-  va_end(va);
+    va_start(va, str);
+    len = vsnprintf(buf, sizeof buf, str, va);
+    va_end(va);
 
-  if (len >= sizeof buf)
-    len = sizeof buf - 1;
+    if (len >= sizeof buf)
+	len = sizeof buf - 1;
 
-  __serial_write(NULL, buf, len);
+    __serial_write(NULL, buf, len);
 }
 
 #else
 
 static inline void debug(const char *str, ...)
 {
-  (void)str;
+    (void)str;
 }
 
 #endif
