09fc07ad2a0b341c6901b01c7ebb0c26e07922e6
H. Peter Anvin <hpa@zytor.com>
2009-05-29 15:10:25 -0700

    Run Nindent on com32/lib/sys/screensize.c
    Automatically reformat com32/lib/sys/screensize.c using Nindent.
    
    Do this for all files except HDT, gPXE and externally maintained
    libraries (zlib, tinyjpeg, libpng).
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- com32/lib/sys/screensize.c
+++ com32/lib/sys/screensize.c
@@ -4,20 +4,20 @@
 
 int getscreensize(int fd, int *rows, int *cols)
 {
-  struct file_info *fp = &__file_info[fd];
+    struct file_info *fp = &__file_info[fd];
 
-  if ( fd >= NFILES || !fp->iop ) {
-    errno = EBADF;
-    return -1;
-  }
+    if (fd >= NFILES || !fp->iop) {
+	errno = EBADF;
+	return -1;
+    }
 
-  *rows = fp->o.rows;
-  *cols = fp->o.cols;
+    *rows = fp->o.rows;
+    *cols = fp->o.cols;
 
-  if (!rows || !cols) {
-    errno = ENOTTY;
-    return -1;
-  }
+    if (!rows || !cols) {
+	errno = ENOTTY;
+	return -1;
+    }
 
-  return 0;
+    return 0;
 }
