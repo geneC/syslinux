6ac16e0040e436037e5b68015bc9a8c48286d05a
H. Peter Anvin <hpa@zytor.com>
2009-05-29 15:10:22 -0700

    Run Nindent on com32/include/stdint.h
    Automatically reformat com32/include/stdint.h using Nindent.
    
    Do this for all files except HDT, gPXE and externally maintained
    libraries (zlib, tinyjpeg, libpng).
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- com32/include/stdint.h
+++ com32/include/stdint.h
@@ -7,49 +7,49 @@
 
 /* Exact types */
 
-typedef signed char		int8_t;
-typedef signed short		int16_t;
-typedef signed int		int32_t;
-typedef signed long long	int64_t;
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+typedef signed long long int64_t;
 
-typedef unsigned char		uint8_t;
-typedef unsigned short		uint16_t;
-typedef unsigned int		uint32_t;
-typedef unsigned long long	uint64_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
 
 /* Small types */
 
-typedef signed char		int_least8_t;
-typedef signed short		int_least16_t;
-typedef signed int		int_least32_t;
-typedef signed long long	int_least64_t;
+typedef signed char int_least8_t;
+typedef signed short int_least16_t;
+typedef signed int int_least32_t;
+typedef signed long long int_least64_t;
 
-typedef unsigned char		uint_least8_t;
-typedef unsigned short		uint_least16_t;
-typedef unsigned int		uint_least32_t;
-typedef unsigned long long	uint_least64_t;
+typedef unsigned char uint_least8_t;
+typedef unsigned short uint_least16_t;
+typedef unsigned int uint_least32_t;
+typedef unsigned long long uint_least64_t;
 
 /* Fast types */
 
-typedef signed char		int_fast8_t;
-typedef signed short		int_fast16_t;
-typedef signed int		int_fast32_t;
-typedef signed long long	int_fast64_t;
+typedef signed char int_fast8_t;
+typedef signed short int_fast16_t;
+typedef signed int int_fast32_t;
+typedef signed long long int_fast64_t;
 
-typedef unsigned char		uint_fast8_t;
-typedef unsigned short		uint_fast16_t;
-typedef unsigned int		uint_fast32_t;
-typedef unsigned long long	uint_fast64_t;
+typedef unsigned char uint_fast8_t;
+typedef unsigned short uint_fast16_t;
+typedef unsigned int uint_fast32_t;
+typedef unsigned long long uint_fast64_t;
 
 /* Pointer types */
 
-typedef int32_t			intptr_t;
-typedef uint32_t		uintptr_t;
+typedef int32_t intptr_t;
+typedef uint32_t uintptr_t;
 
 /* Maximal types */
 
-typedef int64_t			intmax_t;
-typedef uint64_t		uintmax_t;
+typedef int64_t intmax_t;
+typedef uint64_t uintmax_t;
 
 /*
  * To be strictly correct...
