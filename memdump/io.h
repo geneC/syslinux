93d2d1f5e8454645be7977656fce4f07513cde38
H. Peter Anvin <hpa@zytor.com>
2009-05-29 15:10:36 -0700

    Run Nindent on memdump/io.h
    Automatically reformat memdump/io.h using Nindent.
    
    Do this for all files except HDT, gPXE and externally maintained
    libraries (zlib, tinyjpeg, libpng).
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- memdump/io.h
+++ memdump/io.h
@@ -3,38 +3,38 @@
 
 static inline void outb(unsigned char v, unsigned short p)
 {
-  asm volatile("outb %1,%0" : : "d" (p), "a" (v));
+    asm volatile ("outb %1,%0"::"d" (p), "a"(v));
 }
 
 static inline unsigned char inb(unsigned short p)
 {
-  unsigned char v;
-  asm volatile("inb %1,%0" : "=a" (v) : "d" (p));
-  return v;
+    unsigned char v;
+    asm volatile ("inb %1,%0":"=a" (v):"d"(p));
+    return v;
 }
 
 static inline void outw(unsigned short v, unsigned short p)
 {
-  asm volatile("outw %1,%0" : : "d" (p), "a" (v));
+    asm volatile ("outw %1,%0"::"d" (p), "a"(v));
 }
 
 static inline unsigned short inw(unsigned short p)
 {
-  unsigned short v;
-  asm volatile("inw %1,%0" : "=a" (v) : "d" (p));
-  return v;
+    unsigned short v;
+    asm volatile ("inw %1,%0":"=a" (v):"d"(p));
+    return v;
 }
 
 static inline void outl(unsigned int v, unsigned short p)
 {
-  asm volatile("outl %1,%0" : : "d" (p), "a" (v));
+    asm volatile ("outl %1,%0"::"d" (p), "a"(v));
 }
 
 static inline unsigned int inl(unsigned short p)
 {
-  unsigned int v;
-  asm volatile("inl %1,%0" : "=a" (v) : "d" (p));
-  return v;
+    unsigned int v;
+    asm volatile ("inl %1,%0":"=a" (v):"d"(p));
+    return v;
 }
 
 #endif
