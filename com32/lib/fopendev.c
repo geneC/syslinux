375f8dad56f3a39953a533686f83dfce364a4915
H. Peter Anvin <hpa@zytor.com>
2009-05-29 15:10:23 -0700

    Run Nindent on com32/lib/fopendev.c
    Automatically reformat com32/lib/fopendev.c using Nindent.
    
    Do this for all files except HDT, gPXE and externally maintained
    libraries (zlib, tinyjpeg, libpng).
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- com32/lib/fopendev.c
+++ com32/lib/fopendev.c
@@ -6,38 +6,38 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-FILE *fopendev(const struct dev_info *dev, const char *mode)
+FILE *fopendev(const struct dev_info * dev, const char *mode)
 {
-  int flags = O_RDONLY;
-  int plus = 0;
-  int fd;
+    int flags = O_RDONLY;
+    int plus = 0;
+    int fd;
 
-  while ( *mode ) {
-    switch ( *mode ) {
-    case 'r':
-      flags = O_RDONLY;
-      break;
-    case 'w':
-      flags = O_WRONLY|O_CREAT|O_TRUNC;
-      break;
-    case 'a':
-      flags = O_WRONLY|O_CREAT|O_APPEND;
-      break;
-    case '+':
-      plus = 1;
-      break;
+    while (*mode) {
+	switch (*mode) {
+	case 'r':
+	    flags = O_RDONLY;
+	    break;
+	case 'w':
+	    flags = O_WRONLY | O_CREAT | O_TRUNC;
+	    break;
+	case 'a':
+	    flags = O_WRONLY | O_CREAT | O_APPEND;
+	    break;
+	case '+':
+	    plus = 1;
+	    break;
+	}
+	mode++;
     }
-    mode++;
-  }
 
-  if ( plus ) {
-    flags = (flags & ~(O_RDONLY|O_WRONLY)) | O_RDWR;
-  }
+    if (plus) {
+	flags = (flags & ~(O_RDONLY | O_WRONLY)) | O_RDWR;
+    }
 
-  fd = opendev(file, flags);
+    fd = opendev(file, flags);
 
-  if ( fd < 0 )
-    return NULL;
-  else
-    return fdopen(fd, mode);
+    if (fd < 0)
+	return NULL;
+    else
+	return fdopen(fd, mode);
 }
