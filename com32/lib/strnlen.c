1bec0c0b1639568a18702054413b567345eb64b3
H. Peter Anvin <hpa@zytor.com>
2009-05-29 15:10:24 -0700

    Run Nindent on com32/lib/strnlen.c
    Automatically reformat com32/lib/strnlen.c using Nindent.
    
    Do this for all files except HDT, gPXE and externally maintained
    libraries (zlib, tinyjpeg, libpng).
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- com32/lib/strnlen.c
+++ com32/lib/strnlen.c
@@ -6,8 +6,8 @@
 
 size_t strnlen(const char *s, size_t n)
 {
-  const char *ss = s;
-  while ( n-- && *ss )
-    ss++;
-  return ss-s;
+    const char *ss = s;
+    while (n-- && *ss)
+	ss++;
+    return ss - s;
 }
