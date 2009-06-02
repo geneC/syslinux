210395ef1babd423194a5c574b491324d567ab6b
H. Peter Anvin <hpa@zytor.com>
2009-06-02 07:36:43 -0700

    Export the cache_seg to 32-bit code as core_cache_buf
    Export the cache_seg to 32-bit code as core_cache_buf, so we can port
    the cache to 32-bit code without porting its 16-bit clients immediately.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- core/include/core.h
+++ core/include/core.h
@@ -4,6 +4,8 @@
 #include <com32.h>
 
 extern char core_xfer_buf[65536];
+extern char core_cache_buf[65536];
+
 void __cdecl core_intcall(uint8_t, const com32sys_t *, com32sys_t *);
 void __cdecl core_farcall(uint32_t, const com32sys_t *, com32sys_t *);
 int __cdecl core_cfarcall(uint32_t, const void *, uint32_t);
