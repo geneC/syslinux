be65e736d7ce7b1961bab7000eb7789c8ba35ef3
H. Peter Anvin <hpa@zytor.com>
2009-06-02 16:44:41 -0700

    core: add new __lowmem macro to allocate a static lowmem buffer
    Add a new __lowmem macro that can be applied to an uninitialized
    static (or global) object, which allocates it in a new .lowmem section
    allocated below the 1 MB boundary.  Keep in mind that low memory is
    precious!
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- core/include/core.h
+++ core/include/core.h
@@ -1,6 +1,7 @@
 #ifndef CORE_H
 #define CORE_H
 
+#include <klibc/compiler.h>
 #include <com32.h>
 
 extern char core_xfer_buf[65536];
@@ -11,5 +12,7 @@ void __cdecl core_farcall(uint32_t, const com32sys_t *, com32sys_t *);
 int __cdecl core_cfarcall(uint32_t, const void *, uint32_t);
 
 void call16(void (*)(void), const com32sys_t *, com32sys_t *);
+
+#define __lowmem __attribute((nocommon,section(".lowmem")))
 
 #endif /* CORE_H */
