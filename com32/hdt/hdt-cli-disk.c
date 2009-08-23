6f74f1b4edbed5af2e279f31516024c01a2d4bd3
Pierre-Alexandre Meyer <pierre@mouraf.org>
2009-08-23 14:31:53 -0700

    hdt: enhance user experience (CLI)
    Some users have reported that reading the CLI can be difficult,
    especially when spitting out a lot of information.
    
    This patch:
    
      * adds a missing reset_more_printf() in the disk CLI
      * reduces the total scrolling to 20 lines (seems better in my testing)
      * removes a reset_more_printf() in the show disk function. This can potentially
        be bad when doing a lot of show disk 0xXX (especially after 4 in a row)
        but enhances the display when running show disks with lots of disks.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- com32/hdt/hdt-cli-disk.c
+++ com32/hdt/hdt-cli-disk.c
@@ -99,7 +99,6 @@ static void show_partition_information(struct driveinfo *drive_info,
 void main_show_disk(int argc, char **argv,
 		    struct s_hardware *hardware)
 {
-	reset_more_printf();
 	if (!argc) {
 		more_printf("Which disk?\n");
 		return;
@@ -145,6 +144,8 @@ void main_show_disk(int argc, char **argv,
 		}
 		fprintf(stderr, "\n");
 	}
+
+	more_printf("\n");
 }
 
 void main_show_disks(int argc __unused, char **argv __unused,
@@ -167,6 +168,7 @@ void disks_summary(int argc __unused, char** argv __unused,
 	int i = -1;
 
 	detect_disks(hardware);
+	reset_more_printf();
 
 	for (int drive = 0x80; drive < 0xff; drive++) {
 		i++;
