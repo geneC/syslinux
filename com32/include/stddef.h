40cf260af8496f44958216ae34807f2ea74c76e5
H. Peter Anvin <hpa@zytor.com>
2009-09-08 21:05:24 -0700

    stddef.h: add container_of()
    Add the container_of() macro.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- com32/include/stddef.h
+++ com32/include/stddef.h
@@ -21,4 +21,12 @@
 #undef offsetof
 #define offsetof(t,m) ((size_t)&((t *)0)->m)
 
+#undef container_of
+/*
+ * The container_of construct: if p is a pointer to member m of
+ * container class c, then return a pointer to the container of which
+ * *p is a member.
+ */
+#define container_of(p, c, m) ((c *)((char *)(p) - offsetof(c,m)))
+
 #endif /* _STDDEF_H */
