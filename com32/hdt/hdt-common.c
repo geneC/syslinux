9b74afd77bc99b65021c7b10d6298bbeff888b09
Erwan Velu <erwan.velu@free.fr>
2009-11-26 20:14:18 +0100

    hdt: Fixing typo in variable's name
    Impact: Building is working again
    
    Ooops ....

--- com32/hdt/hdt-common.c
+++ com32/hdt/hdt-common.c
@@ -90,7 +90,7 @@ void detect_parameters(const int argc, const char *argv[],
 	    convert_isolinux_filename(hardware->reboot_label, hardware);
 	} else if (!strncmp(argv[i], "vesa", 4)) {
 	    vesamode = true;
-	    max_console_lines = CLI_VESA_MAX_LINES;
+	    max_console_lines = MAX_CLI_LINES;
 	    /* If the user defines a background image */
 	    if (!strncmp(argv[i], "vesa=", 5)) {
 		strncpy(hardware->vesa_background, argv[i] + 5,
@@ -98,7 +98,7 @@ void detect_parameters(const int argc, const char *argv[],
 	    }
 	} else if (!strncmp(argv[i], "novesa", 6)) {
 	    vesamode = false;
-	    max_console_lines = CLI_MAX_LINES;
+	    max_console_lines = MAX_VESA_CLI_LINES;
 	} else if (!strncmp(argv[i], "auto=", 5)) {
 	    /* The auto= parameter is separated in several argv[]
 	     * as it can contains spaces.
