ca0b7b54ad00ff1d6d465cc1cf0c307293d721cc
H. Peter Anvin <hpa@zytor.com>
2010-01-10 12:53:51 -0800

    com32/lib: fix warning in str*cmp()
    Shut up a warning about initializing pointer across signedness.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- com32/lib/strncasecmp.c
+++ com32/lib/strncasecmp.c
@@ -7,7 +7,8 @@
 
 int strncasecmp(const char *s1, const char *s2, size_t n)
 {
-    const unsigned char *c1 = s1, *c2 = s2;
+    const unsigned char *c1 = (const unsigned char *)s1;
+    const unsigned char *c2 = (const unsigned char *)s2;
     unsigned char ch;
     int d = 0;
 
