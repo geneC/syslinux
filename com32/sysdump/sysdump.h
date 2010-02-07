7def7a90e13cfb9a25fdb89029c977653a47e04f
H. Peter Anvin <hpa@zytor.com>
2010-02-07 00:09:31 -0800

    sysdump: dump CPUID information
    Dump CPUID information in as generic of a way as is possible, given
    the ugliness in certain places.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- com32/sysdump/sysdump.h
+++ com32/sysdump/sysdump.h
@@ -4,8 +4,10 @@
 struct backend;
 
 void dump_memory_map(struct backend *);
+void snapshot_lowmem(void);
 void dump_memory(struct backend *);
 void dump_dmi(struct backend *);
+void dump_cpuid(struct backend *);
 void dump_pci(struct backend *);
 void dump_vesa_tables(struct backend *);
 
