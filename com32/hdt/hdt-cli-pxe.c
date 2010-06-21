f6da697067326409b4b70096df9351126acb7c8e
Erwan Velu <erwanaliasr1@gmail.com>
2010-06-21 22:24:21 +0200

    Fixing compilation warning
    Reported by Genne Cumm, some more_printf calls generates gcc warning.

--- com32/hdt/hdt-cli-pxe.c
+++ com32/hdt/hdt-cli-pxe.c
@@ -66,14 +66,14 @@ void main_show_pxe(int argc __unused, char **argv __unused,
 	snprintf(buffer, sizeof(buffer),
 		 " PCI Bus pos. : %02x:%02x.%02x\n", p->pci_bus,
 		 p->pci_dev, p->pci_func);
-	more_printf(buffer);
+	more_printf("%s", buffer);
     } else {
 	snprintf(buffer, sizeof(buffer), " Manufacturer : %s \n",
 		 p->pci_device->dev_info->vendor_name);
-	more_printf(buffer);
+	more_printf("%s", buffer);
 	snprintf(buffer, sizeof(buffer), " Product      : %s \n",
 		 p->pci_device->dev_info->product_name);
-	more_printf(buffer);
+	more_printf("%s", buffer);
     }
     more_printf(" Addresses    : %d.%d.%d.%d @ %s\n", p->ip_addr[0],
 		p->ip_addr[1], p->ip_addr[2], p->ip_addr[3], p->mac_addr);
