a6261fd6112c54f994f6e7a3f3eca606cc2fdf8d
Feng Tang <feng.tang@intel.com>
2010-06-22 14:02:21 +0800

    elflink: modify build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 make -j 4 || exit 1
 cp extlinux/extlinux /home/feng/test/syslinux/
 cp com32/elflink/modules/*.c32 /home/feng/test/syslinux/cd/
+cp memdisk/memdisk /home/feng/test/syslinux/cd/
 sudo extlinux/extlinux -i /home/feng/test/syslinux/cd/
 qemu -hda /home/feng/test/syslinux/test.img
