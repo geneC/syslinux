f83c57717377fa74c89d27e72bf138d4265731fb
Feng Tang <feng.tang@intel.com>
2010-06-30 14:11:21 +0800

    elflink: add several files to com32/elflink/modules
    They are meminfo/pcitest/dir/vesainfo/cpuid/cpuidtest. But not
    all of their .c32 works.
    
    One note from vesainfo.c32 is, we need change all cs_bounce usage
    to lmalloc/free

---
+++ com32/elflink/modules/cpuidtest.c
@@ -0,0 +1,140 @@
+/* ----------------------------------------------------------------------- *
+ *
+ *   Copyright 2006 Erwan Velu - All Rights Reserved
+ *
+ *   Permission is hereby granted, free of charge, to any person
+ *   obtaining a copy of this software and associated documentation
+ *   files (the "Software"), to deal in the Software without
+ *   restriction, including without limitation the rights to use,
+ *   copy, modify, merge, publish, distribute, sublicense, and/or
+ *   sell copies of the Software, and to permit persons to whom
+ *   the Software is furnished to do so, subject to the following
+ *   conditions:
+ *
+ *   The above copyright notice and this permission notice shall
+ *   be included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *   OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * -----------------------------------------------------------------------
+*/
+
+/*
+ * cpuidtest.c
+ *
+ * A CPUID demo program using libcom32
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <console.h>
+#include "cpuid.h"
+
+#include <sys/module.h>
+
+char display_line;
+
+static int cpuidtest_main(void)
+{
+    s_cpu cpu;
+
+    for (;;) {
+	detect_cpu(&cpu);
+	printf("Vendor      = %s\n", cpu.vendor);
+	printf("Model       = %s\n", cpu.model);
+	printf("Vendor ID   = %d\n", cpu.vendor_id);
+	printf("Family      = %d\n", cpu.family);
+	printf("Model ID    = %d\n", cpu.model_id);
+	printf("Stepping    = %d\n", cpu.stepping);
+	printf("Flags       = ");
+	if (cpu.flags.fpu)
+	    printf("fpu ");
+	if (cpu.flags.vme)
+	    printf("vme ");
+	if (cpu.flags.de)
+	    printf("de ");
+	if (cpu.flags.pse)
+	    printf("pse ");
+	if (cpu.flags.tsc)
+	    printf("tsc ");
+	if (cpu.flags.msr)
+	    printf("msr ");
+	if (cpu.flags.pae)
+	    printf("pae ");
+	if (cpu.flags.mce)
+	    printf("mce ");
+	if (cpu.flags.cx8)
+	    printf("cx8 ");
+	if (cpu.flags.apic)
+	    printf("apic ");
+	if (cpu.flags.sep)
+	    printf("sep ");
+	if (cpu.flags.mtrr)
+	    printf("mtrr ");
+	if (cpu.flags.pge)
+	    printf("pge ");
+	if (cpu.flags.mca)
+	    printf("mca ");
+	if (cpu.flags.cmov)
+	    printf("cmov ");
+	if (cpu.flags.pat)
+	    printf("pat ");
+	if (cpu.flags.pse_36)
+	    printf("pse_36 ");
+	if (cpu.flags.psn)
+	    printf("psn ");
+	if (cpu.flags.clflsh)
+	    printf("clflsh ");
+	if (cpu.flags.dts)
+	    printf("dts ");
+	if (cpu.flags.acpi)
+	    printf("acpi ");
+	if (cpu.flags.mmx)
+	    printf("mmx ");
+	if (cpu.flags.sse)
+	    printf("sse ");
+	if (cpu.flags.sse2)
+	    printf("sse2 ");
+	if (cpu.flags.ss)
+	    printf("ss ");
+	if (cpu.flags.htt)
+	    printf("ht ");
+	if (cpu.flags.acc)
+	    printf("acc ");
+	if (cpu.flags.syscall)
+	    printf("syscall ");
+	if (cpu.flags.mp)
+	    printf("mp ");
+	if (cpu.flags.nx)
+	    printf("nx ");
+	if (cpu.flags.mmxext)
+	    printf("mmxext ");
+	if (cpu.flags.lm)
+	    printf("lm ");
+	if (cpu.flags.nowext)
+	    printf("3dnowext ");
+	if (cpu.flags.now)
+	    printf("3dnow! ");
+	if (cpu.flags.vmx)
+	    printf("vmx ");
+	if (cpu.flags.svm)
+	    printf("svm ");
+	printf("\n");
+	printf("SMP         = ");
+	if (cpu.flags.smp)
+	    printf("yes\n");
+	else
+	    printf("no\n");
+	break;
+    }
+
+    return 0;
+}
+MODULE_MAIN(cpuidtest_main);
---
+++ com32/elflink/modules/meminfo.c
@@ -0,0 +1,126 @@
+/* ----------------------------------------------------------------------- *
+ *
+ *   Copyright 2008 H. Peter Anvin - All Rights Reserved
+ *   Copyright 2009 Intel Corporation; author: H. Peter Anvin
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ *   Boston MA 02110-1301, USA; either version 2 of the License, or
+ *   (at your option) any later version; incorporated herein by reference.
+ *
+ * ----------------------------------------------------------------------- */
+
+/*
+ * meminfo.c
+ *
+ * Dump the memory map of the system
+ */
+#include <inttypes.h>
+#include <stdio.h>
+#include <string.h>
+#include <console.h>
+#include <com32.h>
+
+#include <sys/module.h>
+
+struct e820_data {
+    uint64_t base;
+    uint64_t len;
+    uint32_t type;
+    uint32_t extattr;
+} __attribute__ ((packed));
+
+static const char *const e820_types[] = {
+    "usable",
+    "reserved",
+    "ACPI reclaim",
+    "ACPI NVS",
+    "unusable",
+};
+
+static void dump_e820(void)
+{
+    com32sys_t ireg, oreg;
+    struct e820_data ed;
+    uint32_t type;
+
+    memset(&ireg, 0, sizeof ireg);
+
+    ireg.eax.w[0] = 0xe820;
+    ireg.edx.l = 0x534d4150;
+    ireg.ecx.l = sizeof(struct e820_data);
+    ireg.edi.w[0] = OFFS(__com32.cs_bounce);
+    ireg.es = SEG(__com32.cs_bounce);
+
+    memset(&ed, 0, sizeof ed);
+    ed.extattr = 1;
+
+    do {
+	memcpy(__com32.cs_bounce, &ed, sizeof ed);
+
+	__intcall(0x15, &ireg, &oreg);
+	if (oreg.eflags.l & EFLAGS_CF ||
+	    oreg.eax.l != 0x534d4150 || oreg.ecx.l < 20)
+	    break;
+
+	memcpy(&ed, __com32.cs_bounce, sizeof ed);
+
+	if (oreg.ecx.l >= 24) {
+	    /* ebx base length end type */
+	    printf("%8x %016llx %016llx %016llx %d [%x]",
+		   ireg.ebx.l, ed.base, ed.len, ed.base + ed.len, ed.type,
+		   ed.extattr);
+	} else {
+	    /* ebx base length end */
+	    printf("%8x %016llx %016llx %016llx %d [-]",
+		   ireg.ebx.l, ed.base, ed.len, ed.base + ed.len, ed.type);
+	    ed.extattr = 1;
+	}
+
+	type = ed.type - 1;
+	if (type < sizeof(e820_types) / sizeof(e820_types[0]))
+	    printf(" %s", e820_types[type]);
+
+	putchar('\n');
+
+	ireg.ebx.l = oreg.ebx.l;
+    } while (ireg.ebx.l);
+}
+
+static void dump_legacy(void)
+{
+    com32sys_t ireg, oreg;
+    uint16_t dosram = *(uint16_t *) 0x413;
+    struct {
+	uint16_t offs, seg;
+    } *const ivt = (void *)0;
+
+    memset(&ireg, 0, sizeof ireg);
+
+    __intcall(0x12, &ireg, &oreg);
+
+    printf
+	("INT 15h = %04x:%04x  DOS RAM: %dK (0x%05x)  INT 12h: %dK (0x%05x)\n",
+	 ivt[0x15].seg, ivt[0x15].offs, dosram, dosram << 10, oreg.eax.w[0],
+	 oreg.eax.w[0] << 10);
+
+    ireg.eax.b[1] = 0x88;
+    __intcall(0x15, &ireg, &oreg);
+
+    printf("INT 15 88: 0x%04x (%uK)  ", oreg.eax.w[0], oreg.eax.w[0]);
+
+    ireg.eax.w[0] = 0xe801;
+    __intcall(0x15, &ireg, &oreg);
+
+    printf("INT 15 E801: 0x%04x (%uK) 0x%04x (%uK)\n",
+	   oreg.ecx.w[0], oreg.ecx.w[0], oreg.edx.w[0], oreg.edx.w[0] << 6);
+}
+
+static int meminfo_main(void)
+{
+    dump_legacy();
+    dump_e820();
+    return 0;
+}
+MODULE_MAIN(meminfo_main);
