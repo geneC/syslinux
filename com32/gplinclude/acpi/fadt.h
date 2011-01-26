b8e7692e6f269dab73662ab430c04de04dbce946
Erwan Velu <erwanaliasr1@gmail.com>
2011-01-26 23:41:27 +0100

    ACPI: Fixing copyright

--- com32/gplinclude/acpi/fadt.h
+++ com32/gplinclude/acpi/fadt.h
@@ -1,6 +1,6 @@
 /* ----------------------------------------------------------------------- *
  *
- *   Copyright 2009 Erwan Velu - All Rights Reserved
+ *   Copyright 2009-2011 Erwan Velu - All Rights Reserved
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
--- com32/gpllib/acpi/sbst.c
+++ com32/gpllib/acpi/sbst.c
@@ -1,6 +1,6 @@
 /* ----------------------------------------------------------------------- *
  *
- *   Copyright 2009 Erwan Velu - All Rights Reserved
+ *   Copyright 2009-2011 Erwan Velu - All Rights Reserved
  *
  *   Permission is hereby granted, free of charge, to any person
  *   obtaining a copy of this software and associated documentation
