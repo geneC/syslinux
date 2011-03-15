832aa7c75ba6a885566d708e385140edf5a5df31
Matt Fleming <matt.fleming@linux.intel.com>
2011-03-15 21:38:45 +0000

    ldlinux: Fix the following warnings
    ldlinux.c: In function ‘enter_cmdline’:
    ldlinux.c:15:23: warning: unused variable ‘aux’
    ldlinux.c: In function ‘ldlinux_main’:
    ldlinux.c:75:5: warning: implicit declaration of function ‘kaboom’
    
    Signed-off-by: Matt Fleming <matt.fleming@linux.intel.com>

--- com32/elflink/ldlinux/ldlinux.c
+++ com32/elflink/ldlinux/ldlinux.c
@@ -1,6 +1,7 @@
 #include <linux/list.h>
 #include <sys/times.h>
 #include <stdbool.h>
+#include <core.h>
 #include <core-elf.h>
 #include "cli.h"
 #include "console.h"
@@ -12,7 +13,6 @@
 
 static void enter_cmdline(void)
 {
-	struct cli_command  *aux;
 	const char *cmdline;
 
 	/* Enter endless command line prompt, should support "exit" */
