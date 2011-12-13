4abae55e14dd03b46dcea04dd9fd2fd3f6fd4f24
H. Peter Anvin <hpa@linux.intel.com>
2011-12-13 15:52:28 -0800

    find-mingw64: yet another set of prefixes to test...
    This seems to be the current set of prefixes used by the Fedora-cross
    project.
    
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- win64/find-mingw64.sh
+++ win64/find-mingw64.sh
@@ -12,6 +12,8 @@ for prefix in \
     x86_64-mingw64msvc- \
     x86_64-mingw32- \
     x86_64-mingw32msvc- \
+    x86_64-w64-mingw32- \
+    x86_64-w64-mingw32msvc- \
     amd64-pc-mingw64- \
     amd64-pc-mingw64msvc- \
     amd64-pc-mingw32- \
@@ -20,6 +22,8 @@ for prefix in \
     amd64-mingw64msvc- \
     amd64-mingw32- \
     amd64-mingw32msvc- \
+    amd64-w64-mingw32- \
+    amd64-w64-mingw32msvc- \
     ; do
     if "${prefix}${cc}" -v > /dev/null 2>&1; then
 	echo "$prefix"
