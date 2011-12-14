46b993449264a2332427a165b4c77e400a053178
H. Peter Anvin <hpa@linux.intel.com>
2011-12-14 14:00:21 -0800

    core: remove redundant zalloc() definition
    Remove a redundant definition of zalloc() which causes a compile
    failure.
    
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- core/mem/malloc.c
+++ core/mem/malloc.c
@@ -201,14 +201,3 @@ void *realloc(void *ptr, size_t size)
 	}
     }
 }
-
-void *zalloc(size_t size)
-{
-    void *ptr;
-
-    ptr = malloc(size);
-    if (ptr)
-	memset(ptr, 0, size);
-
-    return ptr;
-}
