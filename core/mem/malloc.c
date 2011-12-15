89dc4795d95247b36a4dd85cbcd11ca52371c57c
H. Peter Anvin <hpa@linux.intel.com>
2011-12-15 10:47:41 -0800

    Revert "core: remove redundant zalloc() definition"
    This reverts commit 46b993449264a2332427a165b4c77e400a053178.
    
    Build failure was due to a non-clean tree.
    
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- core/mem/malloc.c
+++ core/mem/malloc.c
@@ -201,3 +201,14 @@ void *realloc(void *ptr, size_t size)
 	}
     }
 }
+
+void *zalloc(size_t size)
+{
+    void *ptr;
+
+    ptr = malloc(size);
+    if (ptr)
+	memset(ptr, 0, size);
+
+    return ptr;
+}
