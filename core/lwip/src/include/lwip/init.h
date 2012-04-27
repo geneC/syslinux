3ad59fbce2c5af0b208f2a15bd407512c6885c7a
Gene Cumm <gene.cumm@gmail.com>
2012-04-26 21:27:54 -0400

    core/lwip: update to lwIP-1.4.0
    Also involved moving some calls as certain functions within lwip moved/changed.
    
    Signed-off-by: Gene Cumm <gene.cumm@gmail.com>

--- core/lwip/src/include/lwip/init.h
+++ core/lwip/src/include/lwip/init.h
@@ -41,9 +41,9 @@ extern "C" {
 /** X.x.x: Major version of the stack */
 #define LWIP_VERSION_MAJOR      1U
 /** x.X.x: Minor version of the stack */
-#define LWIP_VERSION_MINOR      3U
+#define LWIP_VERSION_MINOR      4U
 /** x.x.X: Revision of the stack */
-#define LWIP_VERSION_REVISION   2U
+#define LWIP_VERSION_REVISION   0U
 /** For release candidates, this is set to 1..254
   * For official releases, this is set to 255 (LWIP_RC_RELEASE)
   * For development versions (CVS), this is set to 0 (LWIP_RC_DEVELOPMENT) */
--- core/lwip/src/include/lwip/timers.h
+++ core/lwip/src/include/lwip/timers.h
@@ -1,9 +1,3 @@
-/**
- * @file
- * Loop Interface
- *
- */
-
 /*
  * Copyright (c) 2001-2004 Swedish Institute of Computer Science.
  * All rights reserved. 
@@ -33,34 +27,72 @@
  * This file is part of the lwIP TCP/IP stack.
  * 
  * Author: Adam Dunkels <adam@sics.se>
+ *         Simon Goldschmidt
  *
  */
+#ifndef __LWIP_TIMERS_H__
+#define __LWIP_TIMERS_H__
+
 #include "lwip/opt.h"
 
-#if LWIP_HAVE_LOOPIF
+/* Timers are not supported when NO_SYS==1 and NO_SYS_NO_TIMERS==1 */
+#define LWIP_TIMERS (!NO_SYS || (NO_SYS && !NO_SYS_NO_TIMERS))
 
-#include "netif/loopif.h"
-#include "lwip/snmp.h"
+#if LWIP_TIMERS
 
-/**
- * Initialize a lwip network interface structure for a loopback interface
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef LWIP_DEBUG_TIMERNAMES
+#ifdef LWIP_DEBUG
+#define LWIP_DEBUG_TIMERNAMES SYS_DEBUG
+#else /* LWIP_DEBUG */
+#define LWIP_DEBUG_TIMERNAMES 0
+#endif /* LWIP_DEBUG*/
+#endif
+
+/** Function prototype for a timeout callback function. Register such a function
+ * using sys_timeout().
  *
- * @param netif the lwip network interface structure for this loopif
- * @return ERR_OK if the loopif is initialized
- *         ERR_MEM if private data couldn't be allocated
+ * @param arg Additional argument to pass to the function - set up by sys_timeout()
  */
-err_t
-loopif_init(struct netif *netif)
-{
-  /* initialize the snmp variables and counters inside the struct netif
-   * ifSpeed: no assumption can be made!
-   */
-  NETIF_INIT_SNMP(netif, snmp_ifType_softwareLoopback, 0);
+typedef void (* sys_timeout_handler)(void *arg);
 
-  netif->name[0] = 'l';
-  netif->name[1] = 'o';
-  netif->output = netif_loop_output;
-  return ERR_OK;
+struct sys_timeo {
+  struct sys_timeo *next;
+  u32_t time;
+  sys_timeout_handler h;
+  void *arg;
+#if LWIP_DEBUG_TIMERNAMES
+  const char* handler_name;
+#endif /* LWIP_DEBUG_TIMERNAMES */
+};
+
+void sys_timeouts_init(void);
+
+#if LWIP_DEBUG_TIMERNAMES
+void sys_timeout_debug(u32_t msecs, sys_timeout_handler handler, void *arg, const char* handler_name);
+#define sys_timeout(msecs, handler, arg) sys_timeout_debug(msecs, handler, arg, #handler)
+#else /* LWIP_DEBUG_TIMERNAMES */
+void sys_timeout(u32_t msecs, sys_timeout_handler handler, void *arg);
+#endif /* LWIP_DEBUG_TIMERNAMES */
+
+void sys_untimeout(sys_timeout_handler handler, void *arg);
+#if NO_SYS
+void sys_check_timeouts(void);
+void sys_restart_timeouts(void);
+#else /* NO_SYS */
+void sys_timeouts_mbox_fetch(sys_mbox_t *mbox, void **msg);
+#endif /* NO_SYS */
+
+
+#ifdef __cplusplus
 }
+#endif
 
-#endif /* LWIP_HAVE_LOOPIF */
+#endif /* LWIP_TIMERS */
+#endif /* __LWIP_TIMERS_H__ */
