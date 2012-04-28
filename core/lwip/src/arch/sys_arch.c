930e929b202ed292077fb14981e5c6bd4c55651e
Gene Cumm <gene.cumm@gmail.com>
2012-04-27 22:36:16 -0400

    core/pxe: lwip  semaphore changes
    Upstream changed how it called back to the system implementing
    semaphores/mutexes
    
    Signed-off-by: Gene Cumm <gene.cumm@gmail.com>

--- core/lwip/src/arch/sys_arch.c
+++ core/lwip/src/arch/sys_arch.c
@@ -8,77 +8,119 @@ void sys_init(void)
 {
 }
 
-sys_sem_t sys_sem_new(u8_t count)
+err_t sys_sem_new(sys_sem_t *sem, u8_t count)
 {
-    sys_sem_t sem = malloc(sizeof(struct semaphore));
     if (!sem)
-	return NULL;
+	return EINVAL;
+    *sem = malloc(sizeof(struct semaphore));
+    if (!*sem)
+	return ENOMEM;
 
-    sem_init(sem, count);
-    return sem;
+    sem_init(*sem, count);
+    return 0;
 }
 
-void sys_sem_free(sys_sem_t sem)
+void sys_sem_free(sys_sem_t *sem)
 {
-    free(sem);
+    if (!!sem && !!*sem) {
+	sys_sem_set_invalid(sem);
+	free(*sem);
+    }
 }
 
-u32_t sys_arch_sem_wait(sys_sem_t sem, u32_t timeout)
+void sys_sem_set_invalid(sys_sem_t *sem)
+{
+    if (!sem || !*sem)
+	return;
+    sem_set_invalid(*sem);
+}
+
+
+int sys_sem_valid(sys_sem_t *sem)
+{
+    if (!sem || !*sem)
+	return 0;
+    return sem_is_valid(*sem);
+}
+
+u32_t sys_arch_sem_wait(sys_sem_t *sem, u32_t timeout)
 {
     mstime_t rv;
 
-    rv = sem_down(sem, timeout);
+    if (!!sem)
+	return SYS_ARCH_TIMEOUT;
+    rv = sem_down(*sem, timeout);
     if (rv == (mstime_t)-1)
 	return SYS_ARCH_TIMEOUT;
     else
 	return rv;
 }
 
-sys_mbox_t sys_mbox_new(int size)
+err_t sys_mbox_new(sys_mbox_t *mbox, int size)
 {
-    struct mailbox *mbox;
-
-    mbox = malloc(MBOX_BYTES(size));
     if (!mbox)
-	return NULL;
+	return EINVAL;
+    *mbox = malloc(MBOX_BYTES(size));
+    if (!(*mbox))
+	return ENOMEM;
 
-    mbox_init(mbox, size);
-    return mbox;
+    mbox_init(*mbox, size);
+    return 0;
 }
 
-void sys_mbox_free(sys_mbox_t mbox)
+void sys_mbox_free(sys_mbox_t *mbox)
 {
-    free(mbox);
+    if (!!mbox && !!*mbox)
+	free(*mbox);
 }
 
-void sys_mbox_post(sys_mbox_t mbox, void *msg)
+void sys_mbox_post(sys_mbox_t *mbox, void *msg)
 {
-    mbox_post(mbox, msg, 0);
+    if (!!mbox)
+	mbox_post(*mbox, msg, 0);
 }
 
-err_t sys_mbox_trypost(sys_mbox_t mbox, void *msg)
+err_t sys_mbox_trypost(sys_mbox_t *mbox, void *msg)
 {
-    return mbox_post(mbox, msg, -1);
+    if (!mbox)
+	return EINVAL;
+    return mbox_post(*mbox, msg, -1);
 }
 
-u32_t sys_arch_mbox_fetch(sys_mbox_t mbox, void **msg, u32_t timeout)
+u32_t sys_arch_mbox_fetch(sys_mbox_t *mbox, void **msg, u32_t timeout)
 {
     mstime_t rv;
 
-    rv = mbox_fetch(mbox, msg, timeout);
+    if (!mbox)
+	return SYS_MBOX_EMPTY;
+    rv = mbox_fetch(*mbox, msg, timeout);
     if (rv == (mstime_t)-1)
 	return SYS_ARCH_TIMEOUT;
     else
 	return rv;
 }
 
-u32_t sys_arch_mbox_tryfetch(sys_mbox_t mbox, void **msg)
+u32_t sys_arch_mbox_tryfetch(sys_mbox_t *mbox, void **msg)
 {
-    return mbox_fetch(mbox, msg, -1);
+    if (!mbox)
+	return SYS_MBOX_EMPTY;
+    return mbox_fetch(*mbox, msg, -1);
 }
 
-sys_thread_t sys_thread_new(char *name, void (*thread)(void *),
-			     void *arg, int stacksize, int prio)
+void sys_mbox_set_invalid(sys_mbox_t *mbox)
+{
+    if (!!mbox)
+	mbox_set_invalid(*mbox);
+}
+
+int sys_mbox_valid(sys_mbox_t *mbox)
+{
+    return ((!!mbox) && mbox_is_valid(*mbox));
+}
+
+
+sys_thread_t sys_thread_new(const char *name, lwip_thread_fn thread,
+			    void *arg, int stacksize, int prio)
 {
     return start_thread(name, stacksize, prio, thread, arg);
 }
