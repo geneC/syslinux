97a087ea7814db5134e4d5cd40930fa4bd20f2e0
Matt Fleming <matt.fleming@intel.com>
2012-06-08 11:25:42 +0100

    elflink: Fix compiler warning
    Apply the correct cast to fix the following warning,
    
    chainboot.c: In function ‘chainboot_file’:
    chainboot.c:88:7: warning: passing argument 3 of ‘syslinux_add_movelist’ makes integer from pointer without a cast
    ../../../com32/include/syslinux/movebits.h:57:5: note: expected ‘addr_t’ but argument is of type ‘const void * const’
    
    Signed-off-by: Matt Fleming <matt.fleming@intel.com>

--- com32/elflink/ldlinux/chainboot.c
+++ com32/elflink/ldlinux/chainboot.c
@@ -85,7 +85,7 @@ void chainboot_file(const char *file, enum kernel_type type)
     if (sdi->c.filesystem == SYSLINUX_FS_SYSLINUX ||
 	sdi->c.filesystem == SYSLINUX_FS_EXTLINUX) {
 	if (syslinux_add_movelist(&fraglist, 0x800 - 18,
-				  (const void *)sdi->r.esbx, 16))
+				  (addr_t)sdi->r.esbx, 16))
 	    goto bail;
 
 	/* DS:SI points to partition info */
