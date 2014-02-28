a06818de2a4c49bd64a024dd4f4a09a0caf1f0ac
H. Peter Anvin <hpa@linux.intel.com>
2014-02-28 15:48:53 -0800

    core, bios: Move __syslinux_shuffler_size to assembly
    Putting the __syslinux_shuffler_size in a C file by itself caused it
    to not get included in the core, causing failures.  Put it in assembly
    to make sure it really gets included there.
    
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>

--- core/shuffler.c
+++
@@ -1,8 +0,0 @@
-#include "core.h"
-
-#ifdef __FIRMWARE_BIOS__
-
-extern const char __bcopyxx_len[]; /* Linker script absolute symbol */
-const size_t __syslinux_shuffler_size = (size_t)__bcopyxx_len;
-
-#endif /* __FIRMWARE_BIOS__ */
